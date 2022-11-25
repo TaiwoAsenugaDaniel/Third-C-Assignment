3d7d37cd3d86c28663ea9fbb06e6318123549e4a
TaiwoAsenugaDaniel <117589254+TaiwoAsenugaDaniel@users.noreply.github.com>
2022-11-25 22:47:09 -0100

    added changes to the comments

--- self.c
+++ self.c
@@ -4,40 +4,41 @@
 
 int main (){
     FILE *entry; 
-    //open the file self.in 
+    // code to open the file selfDescribing.in 
     entry = fopen("selfDescribing.in", "r");
+
+    // if statement to display error if file cannot be opened
     if (entry==NULL){
-        // exception that if file cannot be opened dsiplay below//
             printf("File cannot be Found or Error in Opening File");
             exit(1);
         }
 
     int testCases;
-   //read first line for the number of testcases
+   // code to read the first line containing number of testcases
     fscanf(entry, "%d", &testCases);
    
     int numberArray[100];
-    int count = 0;
+    int counter = 0;
     int len;
     char selfDescribing = 'T';
 
     for (int i=0; i<testCases; i++){
-    // run the loop according to the number of testcases
+    // code to run the loop according to the number of testcases
         char num[100];
         fscanf(entry, "%s", num);
-    //read the next line from the file and store as variable num
+    // code to read the next line from the file and store as the variable num
         len = strlen(num);
-    // get the length of the text from the file
+    // code to get the length of the text from the file
         for (int j = 0; j < len; j++) {
-    // storing each character into an array
+    // code to store each character into an array
             numberArray[j] =num[j] - '0';
         }
-        for (int step = 0; step< len; step++) {
+        for (int steps = 0; steps < len; steps++) {
             for (int i = 0; i< len; i++) {
-                if (numberArray[i] == step) 
-                count++;
+                if (numberArray[i] == steps) 
+                counter++;
             }
-            if(count == numberArray[step]){
+            if(counter == numberArray[steps]){
                 selfDescribing = 'T';
             }
             else{
@@ -45,7 +46,7 @@ int main (){
                 printf("Not self-describing\n");
                 break;
             }
-            count = 0;
+            counter = 0;
             if (selfDescribing!= 'F'){
                 printf("Self-describing\n");
                 break;
